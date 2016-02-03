2bc5e297ec12d2a20ce332e7fc5f49f8668eb647
knotman90 <davide90.spataro@gmail.com>
2016-02-03 09:28:57 +0000

    Updated i3config

---
+++ i3/launchApp.sh
@@ -0,0 +1,48 @@
+#simple autostart file for i3-wm, you can execute it from i3 config with
+# exec $HOME/bin/auto-start-for-i3-simple
+#
+# xdotool and xmessage must be installed. On BSD use jot instead of
+# seq or install seq from ports.
+
+
+# Max seconds to wait until the process is up
+MAXWAIT=30
+
+# Start the given command and wait until it's visible
+StartProg()
+{
+    "$@" &                  # Handle arguments with whitspaces
+    mypid=$!                # Pid of last background process
+    for i in `seq $MAXWAIT` # count from 1 to MAXWAIT
+    do
+    	if xdotool search --onlyvisible --pid $mypid; then
+    	    return 0
+    	fi
+	sleep 1
+    done
+    xmessage "Error on executing: $@" &
+}
+
+# Start some programs
+#
+# ______________________
+# |          |          |
+# |  emacs   |  chrome  |
+# |          |          |
+# |          |          |
+# |          |----------|
+# |          |  xterm   |
+# |__________|__________|
+
+
+StartProg google-chrome
+i3-msg split h
+sleep 5
+
+StartProg xterm
+i3-msg split v
+sleep 2
+
+
+
+exit 0
