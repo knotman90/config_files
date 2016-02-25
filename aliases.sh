f79558c0c757027cce6de37eefd716f631cb6b27
knotman90 <davide90.spataro@gmail.com>
2016-02-25 13:59:45 +0000

    added alias for terminal and uncrustify config for c++ (google style)

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+# User specific aliases and functions
+
+# ensures that these commands ask confirmation first
+# -i interactive (prompt) -v verbose (explain what is being done)
+alias rm='rm -iv'
+alias cp='cp -iv'
+alias mv='mv -iv'
+#ref:
+#http://www.thegeekstuff.com/2008/10/6-awesome-linux-cd-command-hacks-productivity-tip3-for-geeks/
+
+#Navigate up the directory using only dots
+alias back='cd -' # toggle between the last two directories
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
